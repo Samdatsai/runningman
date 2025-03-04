7caaa8649e7e3040ed0f536121b9194cb858572f
Samdatsai <11155012@mail.hpsh.tp.edu.tw>
2025-03-04 16:54:56 +0800

    Add files via upload

---
+++ runningman/Form1.cs
@@ -0,0 +1,52 @@
+namespace runningman
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int num = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox3.Image = imageList1.Images[num];
+            if (num == 3)
+                num = 0;
+            else
+                num = num + 1;
+
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            timer2.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            timer1.Enabled = true;
+            timer1.Interval = 200;
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            pictureBox3.Left = pictureBox3.Left - 30;
+            if (pictureBox3.Left < -90)
+            {
+                pictureBox3.Left = this.ClientSize.Width;
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            timer2.Enabled = false;
+        }
+    }
+}
